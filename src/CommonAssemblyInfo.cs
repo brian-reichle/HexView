4bc4ea7b8f379fa8a7f921296190e37226b1b6c8
Brian Reichle <bwreichle+git@gmail.com>
2019-01-19 15:16:27 +1100

    Dropped CommonAssemblyInfo.cs.
    It no longer hold anything meaninful anymore anyway.

--- src/CommonAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
